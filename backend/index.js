993733a30172d6c9702fc8753bfa5caffbf9fff7
Indra Putra <indrakoslab@gmail.com>
2018-07-30 13:35:31 +0700

    fix backend/index.js require

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-const Sequelize = Sequelize('sequelize')
+const Sequelize = require('sequelize')
 const sequelize = new Sequelize('musics', 'indra', '1', {
     host: 'localhost',
     dialect: 'mysql',
